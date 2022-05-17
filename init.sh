deafffc8dc3581c1b2352537d8b2e7e1bbf7219c
ktunprasert <ktunprasert@outlook.com>
2022-05-17 19:48:35 +0100

    init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 docker run -it \
 	-v $PWD:/app\
 	-w=/app\
-	node:16-alpine sh -c "yes | npx create-strapi-app@latest strapi-project --no-run --dbclient=postgres --dbhost=db --dbport=5432 --dbname=app --dbusername=root --dbpassword=toor"
+	node:16-alpine sh -c "yes | npx create-strapi-app@latest app --no-run --dbclient=postgres --dbhost=db --dbport=5432 --dbname=app --dbusername=root --dbpassword=toor"
